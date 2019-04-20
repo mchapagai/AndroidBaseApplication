07dfac5c10aa13a48fe74e4e4b201b2842c116f8
“Manorath <mono.chapagai@gmail.com>
2019-04-20 10:20:24 -0400

    Android Arch component defination

---
+++ App/src/main/java/com/example/mchapagai/datasource/BaseDataSource.java
@@ -0,0 +1,4 @@
+package com.example.mchapagai.datasource;
+
+public class BaseDataSource {
+}
---
+++ App/src/main/java/com/example/mchapagai/viewModel/BaseViewModel.java
@@ -0,0 +1,6 @@
+package com.example.mchapagai.viewModel;
+
+import android.arch.lifecycle.ViewModel;
+
+public class BaseViewModel extends ViewModel {
+}
